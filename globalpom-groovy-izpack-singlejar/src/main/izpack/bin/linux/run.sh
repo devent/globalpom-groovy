4a58bce23da6ebfbf04a8da1f65c68f1e5388a0b
Erwin Mueller <erwin.mueller@deventm.org>
2011-09-15 17:57:59 +0200

    globalpom-groovy-izpack-singlejar: update start scripts

--- globalpom-groovy-izpack-singlejar/src/main/izpack/bin/linux/run.sh
+++ globalpom-groovy-izpack-singlejar/src/main/izpack/bin/linux/run.sh
@@ -4,10 +4,7 @@ symlink=`find "$0" -printf "%l"`
 cd "`dirname "${symlink:-$0}"`"
 
 export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on"
-bootstrapversion=${project.custom.bootstrap.version}
-bootstrap="../../bootstrap/bootstrap-core-$bootstrapversion-jar-with-dependencies.jar"
-libs=../../lib
-mainclass="${project.custom.bootstrap.mainclass}"
+mainjar="../../lib/${project.artifactId}-${project.version}-jar-with-dependencies.jar"
 log="-Dlog4j.configuration=file:///$PWD/../../etc/log4j.properties"
 
-java "$log" -jar "$bootstrap" -libs "$libs" -mainclass "$mainclass" $*
+java "$log" -jar "$mainjar" $*
--- globalpom-groovy-izpack-singlejar/src/main/izpack/bin/windows/run.bat
+++ globalpom-groovy-izpack-singlejar/src/main/izpack/bin/windows/run.bat
@@ -1,10 +1,7 @@
 symlink=`find "$0" -printf "%l"`
 cd "`dirname "${symlink:-$0}"`"
 
-set bootstrapversion=${project.custom.bootstrap.version}
-set bootstrap="../../bootstrap/bootstrap-core-$bootstrapversion-jar-with-dependencies.jar"
-set libs=../../lib
-set mainclass="${project.custom.bootstrap.mainclass}"
+set mainjar="..\..\lib\${project.artifactId}-${project.version}-jar-with-dependencies.jar"
 set log="-Dlog4j.configuration=file:///$PWD/../../etc/log4j.properties"
 
-java %log% -jar %bootstrap% -libs %libs% -mainclass %mainclass%
+java "%log%" -jar "%mainjar%"
