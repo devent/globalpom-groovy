d6813248221d98d05c8699c1eb156017a7ce202b
Erwin Mueller <erwin.mueller@deventm.org>
2011-09-27 14:46:27 +0200

    fixes #834: Windows run files not working

--- globalpom-groovy-izpack/src/main/izpack/bin/windows/debug-run.bat
+++ globalpom-groovy-izpack/src/main/izpack/bin/windows/debug-run.bat
@@ -1,10 +1,7 @@
-symlink=`find "$0" -printf "%l"`
-cd "`dirname "${symlink:-$0}"`"
-
 set bootstrapversion=${project.custom.bootstrap.version}
-set bootstrap="../../bootstrap/bootstrap-core-$bootstrapversion-jar-with-dependencies.jar"
-set libs=../../lib
+set bootstrap="bootstrap/bootstrap-core-%bootstrapversion%-jar-with-dependencies.jar"
+set libs=lib
 set mainclass="${project.custom.bootstrap.mainclass}"
-set log="-Dlog4j.configuration=file:///$PWD/../../etc/debug-log4j.properties"
+set log="-Dlog4j.configuration=file:///%CD%/etc/debug-log4j.properties"
 
 java %log% -jar %bootstrap% -libs %libs% -mainclass %mainclass%
