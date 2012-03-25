d1800b393d6ae618fbf37a28a9e976c6285ea614
Erwin Mueller <erwin.mueller@deventm.org>
2012-03-25 10:37:49 +0200

    Set internal frame properties as attributes so they can be adjusted

--- globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/TestInternalFrameUtil.groovy
+++ globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/TestInternalFrameUtil.groovy
@@ -20,6 +20,26 @@ class TestInternalFrameUtil extends TestFrameUtil {
 	def internalFrameSize = new Dimension(300, 200)
 
 	/**
+	 * If the internal frame can be resized, default is <code>true</code>.
+	 */
+	def resizable = true
+
+	/**
+	 * If the internal frame can be closed, default is <code>false</code>.
+	 */
+	def closable = false
+
+	/**
+	 * If the internal frame is maximized, default is <code>true</code>.
+	 */
+	def maximizable = true
+
+	/**
+	 * If the internal frame is iconified, default is <code>true</code>.
+	 */
+	def iconifiable = true
+
+	/**
 	 * Creates a new {@link JFrame} with a {@link JInternalFrame} for the 
 	 * fixture. The component is added to the internal frame.
 	 *
@@ -39,7 +59,7 @@ class TestInternalFrameUtil extends TestFrameUtil {
 	 * @return the created {@link JFrame}.
 	 */
 	def createFrame(def title, def component) {
-		def internalFrame = new JInternalFrame(title, true, false, true, true)
+		def internalFrame = new JInternalFrame(title, resizable, closable, maximizable, iconifiable)
 		internalFrame.setSize internalFrameSize
 		internalFrame.setVisible true
 		internalFrame.setSelected true
