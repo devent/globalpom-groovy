3f86ab1b2cba14bd3157eaf2e5317e6422f5f228
Erwin Mueller <erwin.mueller@deventm.org>
2012-11-10 09:51:57 +0100

    Removed izpack modules
    The izpack modules are now in the
    git://gitpublic@anr-institute.com:globalpom-installation.git repository.

--- globalpom-groovyizpack/src/main/izpack/bin/windows/debug-run.bat
+++
@@ -1,5 +0,0 @@
-set libs=%CD%\lib
-set mainclass="${project.custom.mainclass}"
-set log="-Dlog4j.configuration=file:///%CD%/etc/debug-log4j.properties"
-
-java %log% -cp "%libs%/*"; %mainclass%
--- globalpom-groovyizpack/src/main/izpack/bin/windows/run.bat
+++
@@ -1,5 +0,0 @@
-set libs=%CD%\lib
-set mainclass="${project.custom.mainclass}"
-set log="-Dlog4j.configuration=file:///%CD%/etc/log4j.properties"
-
-start javaw %log% -cp "%libs%/*"; %mainclass%
