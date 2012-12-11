7c0a84367b0e5e8ee94caea004a292dcb9e3210a
Erwin Mueller <erwin.mueller@deventm.org>
2012-12-11 14:35:54 +0100

    Add license header
    #1861

--- globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/ProcessContext.groovy
+++ globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/ProcessContext.groovy
@@ -1,7 +1,20 @@
 /*
- * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * Copyright 2011-2012 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of fdsanalysiscli-socketsyncproject. All rights reserved.
+ * This file is part of globalpom-groovytestutils.
+ *
+ * globalpom-groovytestutils is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * globalpom-groovytestutils is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with globalpom-groovytestutils. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.globalpom.utils
 
