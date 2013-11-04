1918cc8e0664e5baeefd7515a8f3ab809f8ab6c4
Erwin Mueller <erwin.mueller@deventm.org>
2013-11-04 16:50:34 +0100

    Document the arguments

--- globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/frametesting/DialogTestingFactory.groovy
+++ globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/frametesting/DialogTestingFactory.groovy
@@ -11,6 +11,28 @@ interface DialogTestingFactory {
 	/**
 	 * Creates a new dialog testing with the specified arguments.
 	 *
+	 * @param title
+	 * 			  the title of the dialog;
+	 *
+	 * @param size
+	 * 			  the {@link Dimension} size of the dialog;
+	 *
+	 * @param createDialog
+	 * 			  callback to create the {@link JDialog}.
+	 *
+	 * @param createComponent
+	 * 			  callback to create the {@link Component} for the frame;
+	 * 			  the {@link JFrame} is passed as the first argument.
+	 *
+	 * @param setupDialog
+	 * 			  callback to setups the {@link JDialog};
+	 * 			  the {@link JDialog} is passed as the first argument;
+	 * 			  the {@link Component} is passed as the second argument;
+	 *
+	 * @param createFixture
+	 * 			  callback to create the {@link FrameFixture};
+	 * 			  the {@link JDialog} is passed as the first argument;
+	 *
 	 * @return the {@link DialogTesting}.
 	 */
 	DialogTesting create(Map args)
--- globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/frametesting/FrameTestingFactory.groovy
+++ globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/frametesting/FrameTestingFactory.groovy
@@ -11,6 +11,28 @@ interface FrameTestingFactory {
 	/**
 	 * Creates a new frame testing with the specified arguments.
 	 *
+	 * @param title
+	 * 			  the title of the frame;
+	 *
+	 * @param size
+	 * 			  the {@link Dimension} size of the frame;
+	 *
+	 * @param createFrame
+	 * 			  callback to create the {@link JFrame}.
+	 *
+	 * @param createComponent
+	 * 			  callback to create the {@link Component} for the frame;
+	 * 			  the {@link JFrame} is passed as the first argument.
+	 *
+	 * @param setupFrame
+	 * 			  callback to setups the {@link JFrame};
+	 * 			  the {@link JFrame} is passed as the first argument;
+	 * 			  the {@link Component} is passed as the second argument;
+	 *
+	 * @param createFixture
+	 * 			  callback to create the {@link FrameFixture};
+	 * 			  the {@link JFrame} is passed as the first argument;
+	 *
 	 * @return the {@link FrameTesting}.
 	 */
 	FrameTesting create(Map args)
