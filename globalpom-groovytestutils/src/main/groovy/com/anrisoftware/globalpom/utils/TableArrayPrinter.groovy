0d24f9eb263ac5a0464d5a2979d9a91957c82dd7
Erwin Müller <erwin.mueller@deventm.org>
2018-11-26 14:48:30 +0100

    Feature #4160 Update parent, Jenkinsfile and license headers.

--- globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/TableArrayPrinter.groovy
+++ globalpom-groovytestutils/src/main/groovy/com/anrisoftware/globalpom/utils/TableArrayPrinter.groovy
@@ -1,17 +1,24 @@
+/**
+ * Copyright © 2011 Erwin Müller (erwin.mueller@anrisoftware.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.anrisoftware.globalpom.utils
 
 import static org.apache.commons.lang3.Validate.*
 
 import org.apache.commons.lang3.ArrayUtils
 
-/**
- * Print an 2D array as a table.
- *
- * @author Erwin Mueller, erwin.mueller@deventm.org
- * @author Lyubomyr Shaydariv, http://stackoverflow.com/users/166589/lyubomyr-shaydariv
- * @author http://stackoverflow.com/a/11384393
- * @since 1.14
- */
 class TableArrayPrinter {
 
 	private static final URL PROPERTIES_RESOURCE = TableArrayPrinter.class.getResource("/table_array_printer.properties")
